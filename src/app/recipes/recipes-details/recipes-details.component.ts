72e8c3f27b39ed52d0f28c625b05497bb4a22a41
Ryan <ryanjones@Ryans-MacBook-Pro.local>
2017-09-22 14:16:02 +0200

    added dynamic routing for edit buttons in recipe list and details

--- src/app/recipes/recipes-details/recipes-details.component.ts
+++ src/app/recipes/recipes-details/recipes-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-details',
@@ -11,7 +11,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipesDetailsComponent implements OnInit {
   recipe: Recipe;
   id: number
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
@@ -22,5 +22,9 @@ export class RecipesDetailsComponent implements OnInit {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
   }
 }
